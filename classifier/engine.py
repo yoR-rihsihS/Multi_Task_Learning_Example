29669a0eeac11522eee9f68ca42abb65815bffec
Shishir Roy <srshishirroy@gmail.com>
2025-07-18 13:51:29 +0530

    added comments

--- classifier/engine.py
+++ classifier/engine.py
@@ -1,6 +1,18 @@
 import torch
 
 def train_one_epoch(model, data_loader, loss_fn, optimizer, scaler, device):
+    """
+    Trains the given model for one complete epoch over the given dataset
+    Args:
+        - model (torch.nn.Module): The PyTorch model to be trained.
+        - data_loader (torch.utils.data.DataLoader): The data loader for training data.
+        - loss_fn (callable): The loss function used for calculating the error.
+        - optimizer (torch.optim.Optimizer): The algorithm for updating model weights.
+        - scaler (torch.cuda.amp.GradScaler): The gradient scaler for mixed-precision.
+        - device (str): The device (e.g., 'cpu' or 'cuda' or 'cuda:5') to run on.
+    Returns:
+        - dict: A dictionary containing the average loss and accuracy metrics for each of the two model heads for the completed epoch.
+    """
     running_loss_1 = 0
     running_loss_2 = 0
     total_correct_1 = 0
@@ -9,12 +21,12 @@ def train_one_epoch(model, data_loader, loss_fn, optimizer, scaler, device):
     model.train()
     optimizer.zero_grad()
     for image, gt_class_1, gt_class_2 in data_loader:
-        image = image.to(device)
-        gt_class_1 = gt_class_1.to(device)
-        gt_class_2 = gt_class_2.to(device)
+        image = image.to(device)                # shape: [bs, 3, h, w]
+        gt_class_1 = gt_class_1.to(device)      # shape: [bs,]
+        gt_class_2 = gt_class_2.to(device)      # shape: [bs,]
 
         with torch.autocast(device_type='cuda', dtype=torch.float16):
-            logits_1, logits_2 = model(image)
+            logits_1, logits_2 = model(image)                   # shape: [bs, num_classes_1], [bs, num_classes_2]
             loss_1 = loss_fn(logits_1, gt_class_1)
             loss_2 = loss_fn(logits_2, gt_class_2)
             running_loss_1 += loss_1.item() * image.shape[0]
@@ -43,6 +55,16 @@ def train_one_epoch(model, data_loader, loss_fn, optimizer, scaler, device):
 
 @torch.no_grad()
 def evaluate(model, data_loader, loss_fn, device):
+    """
+    Evaluates the given model over the given dataset
+    Args:
+        - model (torch.nn.Module): The PyTorch model to be trained.
+        - data_loader (torch.utils.data.DataLoader): The data loader for training data.
+        - loss_fn (callable): The loss function used for calculating the error.
+        - device (str): The device (e.g., 'cpu' or 'cuda' or 'cuda:5') to run on.
+    Returns:
+        - dict: A dictionary containing the average loss and accuracy metrics for each of the two model heads for the completed epoch.
+    """
     running_loss_1 = 0
     running_loss_2 = 0
     total_correct_1 = 0
